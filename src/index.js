bcf3e3ee11e53fd679bba7a9f048f4b385efcf9b
Michael Anderson <michanders@msn.com>
2018-03-08 22:41:01 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import configureStore from './store/configureStore';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('container')
 );
-
-
